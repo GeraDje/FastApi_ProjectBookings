b037b68baa119ba9b76776effa252343db621194
Gera <sdfdsf>
2023-11-26 14:16:42 +0400

    переходим к написанию роутеров

--- app/hotels/models.py
+++ app/hotels/models.py
@@ -1,19 +1,39 @@
-from sqlalchemy import JSON, Column, Integer, String
+from typing import Optional
+from sqlalchemy import JSON, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, mapped_column, Mapped
 
 from app.database import Base
 
 class Hotels(Base):
-    __tablename__ = 'hotels'
+    __tablename__ = "hotels"
 
-    id: Mapped[int] = mapped_column(Integer,primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str]
     location: Mapped[str]
     services: Mapped[list[str]] = mapped_column(JSON)
     rooms_quantity: Mapped[int]
     image_id: Mapped[int]
+    
+    rooms: Mapped[list["Rooms"]] = relationship(back_populates="hotel")
 
-    # rooms: Mapped[list["Rooms"]] = relationship(back_populates="hotel")
+    def __str__(self):
+        return f"Отель {self.name} {self.location[:30]}"
+    
 
-    # def __str__(self):
-    #     return f"Отель {self.name} {self.location[:30]}"
+class Rooms(Base):
+    __tablename__ = "rooms"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    hotel_id: Mapped[int] = mapped_column(ForeignKey("hotels.id"))
+    name: Mapped[str]
+    description: Mapped[Optional[str]]
+    price: Mapped[int]
+    services: Mapped[list[str]] = mapped_column(JSON)
+    quantity: Mapped[int]
+    image_id: Mapped[int]
+
+    hotel: Mapped["Hotels"] = relationship(back_populates="rooms")
+    bookings: Mapped[list["Bookings"]] = relationship(back_populates="room")
+
+    def __str__(self):
+        return f"Номер {self.name}"
---
+++ app/users/models.py
@@ -0,0 +1,17 @@
+from datetime import date
+from sqlalchemy import  Computed, Date, Integer, ForeignKey
+from sqlalchemy.orm import relationship, mapped_column, Mapped
+
+from app.database import Base
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str]
+    hashed_password: Mapped[str]
+
+    bookings: Mapped[list["Bookings"]] = relationship(back_populates="user")
+
+    def __str__(self):
+        return f"Пользователь {self.email}"
