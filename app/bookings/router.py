5f255994b75a54a7ba7a6ae459596612605a2113
Gera <sdfdsf>
2023-11-28 17:24:53 +0400

    написали первый эндпоинт для получения бронирований

---
+++ app/bookings/router.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+from sqlalchemy import select
+from app.bookings.dao import BookingDAO
+from app.database import async_session_maker
+from app.bookings.models import Bookings
+
+
+router = APIRouter(prefix='/bookings',tags=['Бронирования'])
+
+
+@router.get('')
+async def get_bookings():
+    return await BookingDAO.find_all()
+
+   
