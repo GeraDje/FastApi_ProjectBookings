b02e17ecdc5b5c53d697b77c7b0e185b4c0858a1
Gera <sdfdsf>
2023-11-28 17:23:40 +0400

    написали класс для работы с БД для bookings

---
+++ app/bookings/dao.py
@@ -0,0 +1,12 @@
+from sqlalchemy import select
+from app.bookings.models import Bookings
+from app.database import async_session_maker
+
+class BookingDAO:
+   
+    @classmethod
+    async def find_all(cls):
+        async with async_session_maker() as session:
+            query = select(Bookings.__table__.columns )
+            bookings = await session.execute(query)
+            return bookings.mappings().all()
